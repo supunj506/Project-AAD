3d89b417a14cd6f348f8269c2945ac5516013b39
supunj506 <supunj506@gmail.com>
2023-07-14 18:19:44 +0530

    get string value in request using getParameter()

--- src/main/java/com/example/aad/Student.java
+++ src/main/java/com/example/aad/Student.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Student extends HttpServlet {
     @Override
@@ -16,23 +19,34 @@ public class Student extends HttpServlet {
         String name = req.getParameter("name");
         String address = req.getParameter("address");
 
-        System.out.println(id+"\n"+name+"\n"+address);
+        System.out.println(id + "\n" + name + "\n" + address);
 
-        if(saveStudent(id,name,address)){
-            System.out.println(" ADD Successfully !!!😁");
-
-        }
-        else {
-            System.out.println(" Something Wrong !!!🤔");
-        }
+//        try {
+//            if (saveStudent(id, name, address)) {
+//                System.out.println(" ADD Successfully !!!😁");
+//
+//            } else {
+//                System.out.println(" Something Wrong !!!🤔");
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 
 
     }
 
-    private boolean saveStudent(String id, String name, String address) {
-
-        return false;
-    }
+//    private boolean saveStudent(String id, String name, String address) throws ClassNotFoundException, SQLException {
+//
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/aad", "root", "1234");
+//        PreparedStatement statement = connection.prepareStatement("INSERT INTO student VALUES (?,?,?)");
+//
+//        statement.setString(1, id);
+//        statement.setString(2, name);
+//        statement.setString(3, address);
+//        return statement.executeUpdate() > 0;
+//
+//    }
 
 
 }
