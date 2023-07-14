d2e12814f0d87fee082a18a01eeed45e8ca395a1
supunj506 <supunj506@gmail.com>
2023-07-14 17:41:55 +0530

    add mysql connector dependancy

--- src/main/java/com/example/aad/Student.java
+++ src/main/java/com/example/aad/Student.java
@@ -5,11 +5,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 
 public class Student extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("work fine post method");
+//        System.out.println("work fine post method");
 
         String id = req.getParameter("id");
         String name = req.getParameter("name");
@@ -17,6 +18,21 @@ public class Student extends HttpServlet {
 
         System.out.println(id+"\n"+name+"\n"+address);
 
+        if(saveStudent(id,name,address)){
+            System.out.println(" ADD Successfully !!!😁");
+
+        }
+        else {
+            System.out.println(" Something Wrong !!!🤔");
+        }
+
 
     }
+
+    private boolean saveStudent(String id, String name, String address) {
+
+        return false;
+    }
+
+
 }
