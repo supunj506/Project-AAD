e1a1ed80864048c50e38c969e875a717e690102e
supunj506 <supunj506@gmail.com>
2023-07-14 17:35:39 +0530

    get student details through getParameter method

--- src/main/java/com/example/aad/Student.java
+++ src/main/java/com/example/aad/Student.java
@@ -1,2 +1,22 @@
-package com.example.aad;public class Student {
+package com.example.aad;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Student extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("work fine post method");
+
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+
+        System.out.println(id+"\n"+name+"\n"+address);
+
+
+    }
 }
