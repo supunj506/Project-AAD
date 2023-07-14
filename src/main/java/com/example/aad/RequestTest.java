428cd32da498335e1a8d145b1e158c1e608974fd
supunj506 <supunj506@gmail.com>
2023-07-14 17:18:42 +0530

    map RequestTest class in web xml file

--- src/main/java/com/example/aad/RequestTest.java
+++ src/main/java/com/example/aad/RequestTest.java
@@ -1,2 +1,14 @@
-package com.example.aad;public class RequestTest {
+package com.example.aad;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RequestTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("request come successfully !!!");
+    }
 }
